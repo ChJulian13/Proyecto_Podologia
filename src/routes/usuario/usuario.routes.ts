29ef8951a5404c8b131ef4623ff655f0b1c56c1f
ChJulian13 <christianjulianjimenez13@outlook.com>
2026-03-31 18:34:56 -0600

    refactor(usuarios): proteger endpoints inyectando middleware de autenticación

--- src/routes/usuario/usuario.routes.ts
+++ src/routes/usuario/usuario.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { UsuarioController } from '../../controllers/usuario/usuario.controller.js';
+import { verificarToken } from '../../middleware/auth/auth.middleware.js';
 
 const router = Router();
 const usuarioController = new UsuarioController();
 
-router.get('/clinica/:clinicaId', usuarioController.getAllByClinica);
-router.post('/', usuarioController.create);
-router.patch('/:id', usuarioController.update);
-router.delete('/:id', usuarioController.delete);
+router.get('/clinica/:clinicaId', verificarToken, usuarioController.getAllByClinica);
+router.post('/', verificarToken, usuarioController.create);
+router.patch('/:id', verificarToken, usuarioController.update);
+router.delete('/:id', verificarToken, usuarioController.delete);
 
 export default router;
