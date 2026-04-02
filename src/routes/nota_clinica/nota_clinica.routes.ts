5ad184a5d0db46cd2a64e043a7e5981196f03bc0
ChJulian13 <christianjulianjimenez13@outlook.com>
2026-04-02 16:31:57 -0600

    feat(notas): construir endpoints protegidos para registro y consulta del expediente

---
+++ src/routes/nota_clinica/nota_clinica.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import { NotaClinicaController } from '../../controllers/nota_clinica/nota_clinica.controller.js';
+import { verificarToken } from '../../middleware/auth/auth.middleware.js';
+
+const router = Router();
+const notaController = new NotaClinicaController();
+
+router.get('/paciente/:pacienteId', verificarToken, notaController.getHistorial);
+
+router.post('/', verificarToken, notaController.create);
+router.patch('/:id', verificarToken, notaController.update);
+
+export default router;
