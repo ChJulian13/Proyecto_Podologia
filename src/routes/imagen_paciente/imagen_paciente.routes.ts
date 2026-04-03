0df7a1515257e499897510d27ab8097ab786539e
ChJulian13 <christianjulianjimenez13@outlook.com>
2026-04-02 19:07:29 -0600

    feat(imagenes): crear endpoints multipart/form-data y habilitar carpeta estatica en express

---
+++ src/routes/imagen_paciente/imagen_paciente.routes.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import { ImagenPacienteController } from '../../controllers/imagen_paciente/imagen_paciente.controller.js';
+import { verificarToken } from '../../middleware/auth/auth.middleware.js';
+import { uploadMiddleware } from '../../middleware/upload/upload.middleware.js';
+
+const router = Router();
+const imagenController = new ImagenPacienteController();
+
+// Consulta y Borrado solo requieren el Token
+router.get('/paciente/:pacienteId', verificarToken, imagenController.getByPaciente);
+router.delete('/:id', verificarToken, imagenController.delete);
+
+// Subida: Requiere Token Y pasar por el middleware de Multer
+router.post('/', verificarToken, uploadMiddleware.single('imagen'), imagenController.create);
+
+export default router;
