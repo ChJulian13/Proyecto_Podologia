0dba66899f5f5727174633f702fabb59c087f983
ChJulian13 <christianjulianjimenez13@outlook.com>
2026-05-06 16:41:25 -0600

    Actualizar importación del controlador de agenda rápida con el nombre de archivo corregido

--- src/routes/agenda_rapida/agenda_rapida.routes.ts
+++ src/routes/agenda_rapida/agenda_rapida.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { AgendaRapidaController } from '../../controllers/agenda_rapida/agenda rapida.controller.js';
+import { AgendaRapidaController } from '../../controllers/agenda_rapida/agenda_rapida.controller.js';
 
 const router = Router();
 const agendaRapidaController = new AgendaRapidaController();
