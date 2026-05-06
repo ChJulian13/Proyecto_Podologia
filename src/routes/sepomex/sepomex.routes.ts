76286ac59474e70b797032087ce06297dc8d8b7f
ChJulian13 <christianjulianjimenez13@outlook.com>
2026-05-06 16:44:41 -0600

    chore: remove obsolete routes and controllers

--- src/routes/sepomex/sepomex.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { SepomexController } from '../../controllers/sepomex/sepomex.controller.js';
-
-const router = Router();
-const sepomexController = new SepomexController();
-
-router.get('/cp/:cp', sepomexController.getByCP);
-
-export default router;
